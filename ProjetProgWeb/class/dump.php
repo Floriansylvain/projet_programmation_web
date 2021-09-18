ae7f4d88e7ead1e921b0a75ad98287e1a011c569
Florian SYLVAIN <f.salututo@gmail.com>
2021-09-18 14:13:10 +0200

    🚧 First introduction of these and dump system

--- ProjetProgWeb/class/dump.php
+++ ProjetProgWeb/class/dump.php
@@ -1,22 +1,39 @@
 <?php
 
 class dump {
-    private PDO $pdo;
+    private these $these;
 
     /**
-     * @param PDO $pdo
+     * @param these $these
      */
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
+    public function __construct(these $these) {
+        $this->these = $these;
     }
 
     public function sendThese() {
-        $sql = 'SELECT * FROM theses';
-        $req = $this->pdo->query($sql);
-        while($row = $req->fetch()) {
-            echo "<p>" . $row['author'] . " | " . $row['title'] . "</p>";
-        }
-        $req->closeCursor();
+        $data = [
+            'author' => $this->these->getAuthor(),
+            'id_author' => $this->these->getIdAuthor(),
+            'these_director' => $this->these->getTheseDirector(),
+            'title' => $this->these->getTitle(),
+            'these_director_name_lastname' => $this->these->getTheseDirectorNameLastname(),
+            'id_director' => $this->these->getIdDirector(),
+            'soutenance_establishment' => $this->these->getSoutenanceEstablishment(),
+            'id_establishment' => $this->these->getIdEstablishment(),
+            'discipline' => $this->these->getDiscipline(),
+            'status' => $this->these->getStatus(),
+            'date_first_registration' => $this->these->getDateFirstRegistration(),
+            'date_soutenance' => $this->these->getDateSoutenance(),
+            'language' => $this->these->getLanguage(),
+            'id_these' => $this->these->getIdThese(),
+            'online' => $this->these->isOnline() ? "yes" : "no",
+            'date_publication' => $this->these->getDatePublication(),
+            'date_update' => $this->these->getDateUpdate()
+        ];
+        $sql = "INSERT INTO theses (author, id_author, these_director, title, these_director_name_lastname, id_director, soutenance_establishment, id_establishment, discipline, status, date_first_registration, date_soutenance, language, id_these, online, date_publication, date_update) VALUES (:author, :id_author, :these_director, :title, :these_director_name_lastname, :id_director, :soutenance_establishment, :id_establishment, :discipline, :status, :date_first_registration, :date_soutenance, :language, :id_these, :online, :date_publication, :date_update)";
+        $pdo = new PDO('mysql:host=localhost;dbname=projet_prog_web', 'root', 'root');
+        $stmt= $pdo->prepare($sql);
+        $stmt->execute($data);
     }
 
 }
--- ProjetProgWeb/index.php
+++ ProjetProgWeb/index.php
@@ -1,24 +1,49 @@
 <?php
-    $pdo = new PDO('mysql:host=localhost;dbname=projet_prog_web', 'root', 'root');
 
+require_once("conf.php");
+require_once("class/these.php");
+require_once("class/dump.php");
 
+$these = new these("Florian",
+    "967125",
+    "Ma thèse très sympa",
+    "Didier Deschamps",
+    "Deschamps Didier",
+    "8516243",
+    "IUT Marne la vallée",
+    "128675",
+    "Informatique",
+    "passée",
+    "2001-1-3",
+    "2000-12-24",
+    "en",
+    "2146795",
+    false,
+    "2000-1-1",
+    "2000-1-1"
+);
 
-    $row = 1;
-    if (($handle = fopen("scripts/dump_abes_thesesfr.csv", "r")) !== FALSE) {
-        while (($data = fgetcsv($handle, 10000, ";")) !== FALSE) {
-            $num = count($data);
-            echo "<p>---------------- " . $row .  " -----------------<br /></p>\n";
-            $row++;
-            for ($c=0; $c < $num; $c++) {
-                if ($data[$c]) {
-                    echo $data[$c] . "<br />\n";
-                } else {
-                    echo "NULL<br />\n";
-                }
+$dump = new dump($these);
+$dump->sendThese();
+
+/*
+$row = 1;
+if (($handle = fopen("scripts/dump_abes_thesesfr.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 10000, ";")) !== FALSE) {
+        $num = count($data);
+        echo "<p>---------------- " . $row .  " -----------------<br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            if ($data[$c]) {
+                echo $data[$c] . "<br />\n";
+            } else {
+                echo "NULL<br />\n";
             }
         }
-        fclose($handle);
     }
+    fclose($handle);
+}*/
+
 
 /*
     $sql = 'SELECT * FROM theses';
