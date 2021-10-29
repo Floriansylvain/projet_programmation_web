4cfcf90ffcfef00659d9e7d3e6d612cb3e243046
Florian SYLVAIN <f.salututo@gmail.com>
2021-10-29 10:34:24 +0200

    ✨ Introduced main research page (mobile design)

--- ProjetProgWeb/index.php
+++ ProjetProgWeb/index.php
@@ -5,13 +5,28 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="stylesheet.css">
         <title>Document</title>
     </head>
     <body>
-        <h1>Welcome</h1>
         <?php
+            include("navbar.php");
 
-            echo "Welcome";
+            if (!isset($_POST['author'])) {
+                exit();
+            }
+
+            require_once("class/dump.php");
+
+            $author_name = $_POST['author'];
+
+            $these_array = dump::getTheseByAuthor($author_name);
+
+            $json = json_encode($these_array, JSON_PRETTY_PRINT);
+
+            echo '<pre>';
+            var_dump($json);
+            echo '</pre>';
 
         ?>
     </body>
---
+++ ProjetProgWeb/navbar.php
@@ -0,0 +1,11 @@
+<header>
+    <nav class="navbar">
+        <h1>theses.fr</h1>
+        <form action="index.php" method="POST" class="navbar-input">
+            <input type="text" name="author" placeholder="nom auteur">
+            <button>
+                <img src="assets/search.svg" alt="search">
+            </button>
+        </form>
+    </nav>
+</header>
